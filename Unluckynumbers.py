85a38834a34347be6bfded7aba38c6aee2f4f159
Joubert Lacerda <46083794+JoubertLacerda@users.noreply.github.com>
2019-03-16 13:33:21 +1000

    Add files via upload

---
+++ Unluckynumbers.py
@@ -0,0 +1,11 @@
+## similar to the "FIZ/BUZZ" question
+for x in range(1,21):    
+    if x == 4 or x == 13:
+        print(f"{x} is unlucky")
+    elif x % 2==0:
+        print(f"{x} is Odd ")
+    else:
+        print(f"{x} is even")
+print("\n")
+
+        
